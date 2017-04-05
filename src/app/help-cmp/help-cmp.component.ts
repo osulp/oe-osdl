7887a18efb399d3e8ce6ef11d6b2438880c6084b
marcgr <marc.rempel@oregonstate.edu>
2017-04-05 09:04:39 -0700

    Updated help page with router comp and link to resources

--- src/app/help-cmp/help-cmp.component.ts
+++ src/app/help-cmp/help-cmp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-help-cmp',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpCmpComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
